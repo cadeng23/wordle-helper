6f28788f41c42c231724518f6f58503d8fdccd94
Caden Gustafson <gustafsoncaden@gmail.com>
2022-04-25 13:29:31 -0600

    working on the filter

--- program/Main.py
+++ program/Main.py
@@ -1,9 +1,6 @@
 #Caden Gustafson
 old = []
-words = []
-possible = []
-temp = []
-def read_words():
+def read_words(words):
     file = open("./program/newWords.txt",'r')
     old = file.readlines()
     for line in old:
@@ -41,7 +38,8 @@ def input_one(words):
     print(current)
     return outed, inned, current
 
-def filtered(words, outed, inned, current, possible):
+def filtered(words, outed, inned, current):
+    #Step 1: store the known letters and spots for the letters 
     known = [] #the known letter(s) in the word
     spotKnown = [] #The know indexes that correspond to the word in the same spot on the known list
     for x in current:
@@ -51,38 +49,71 @@ def filtered(words, outed, inned, current, possible):
             else:
                 low = x
             i = current.index(x)
-            known.append(x)
+            known.append(low)
             spotKnown.append(i)
-    s = 0
+    #End Step 1
+    #Step 2: filter through all the words for the letters in the spots that are known
+    temp = []
+    possible = []
+    print(known)
+    print(spotKnown)
     for word in words:
-        if word[spotKnown[s]] == known[s]:
+        spot = spotKnown[0]
+        if known[spot] == word[spotKnown[0]]:
             possible.append(word)
-    s += 1
-    if len(spotKnown) > 1:
-        temp = possible
-        possible = []
-        for word in temp:
-            if word[spotKnown[s]] == known[s]:
-                possible.append(word)
-    s += 1
-    if len(spotKnown) > 2:
-        temp = possible
-        possible = []
-        for word in temp:
-            if word[spotKnown[s]] == known[s]:
-                possible.append(word)
-    s += 1
-    if len(spotKnown) > 3:
-        temp = possible
-        possible = []
-        for word in temp:
-            if word[spotKnown[s]] == known[s]:
-                possible.append(word)
     print(possible)
+    for here in spotKnown:
+        temp = possible
+        possible = []
+        for word in words:
+            spot = spotKnown.index(here)
+            if known[spot] == word[here] and word in temp:
+                possible.append(word)
+    #End Step 2
+    #Step 3: filter out based on the letters that are known not to be in the word
+    print('after step 2 =',possible)
+    temp = possible
+    possible = []
+    c = 0
+    for x in temp:
+        for a in outed:
+            if a not in x:
+                c += 1
+            else: 
+                break
+        if c == len(outed):
+            possible.append(x)
+        c = 0
+    #End Step 3
+    #Step 4: check to see what letters are known but do not have a spot yet
+    temp = possible
+    print('after step 3 =',possible)
+    final = []
+    p = 0
+    print(len(inned))
+    if len(inned) > 0:
+        for x in temp:
+            for i in inned:
+                if i in x:
+                    p += 1
+                else:
+                    break
+            if p == len(inned):
+                final.append(x)
+            p = 0
+    if len(inned) == 0:
+        final = temp
+    #End Step 4
+    t = 1
+    print(final)
+    for f in final:
+        print('answer', t, '=', f)
 
 def main():
-    read_words()
+    words = []
+    words = read_words(words)
+    #print(words)
     outed, inned, current = input_one(words)
-    filtered (words, outed, inned, current, possible)
+    filtered (words, outed, inned, current)
 
 main()
